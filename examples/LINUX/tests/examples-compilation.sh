876afe0b0cb5cd87f0d7fee515d31866c693d46b
gioblu <gioscarab@gmail.com>
2025-10-15 13:10:06 +0200

    examples-compilation.sh test refactored, output style

--- examples/LINUX/tests/examples-compilation.sh
+++ examples/LINUX/tests/examples-compilation.sh
@@ -17,19 +17,30 @@ declare -a tests=(
 	"../OSX/ASCII-table/ASCII-table"
 )
 
+# All expected errors from the compiler
+declare -a errors=(
+	"compilation failed"
+	"unable to open source file"
+	"the source file is too big"
+	"unable to read source file"
+)
+
 echo " "
 echo "bcc compilation test run: examples/LINUX/ "
 echo " "
 
-length=${#tests[@]}
+tests_length=${#tests[@]}
+errors_length=${#errors[@]}
 
 # Try to compile all files in the list above and print the result of the test
-for ((i=1; i<${length} + 1; i++ ));
+for ((i=1; i<${tests_length} + 1; i++ ));
 do
 	result=$(bcc "../${tests[$i - 1]}.biplan" test.bip)
 	code=$?
+	error="unknown"
+	[ $code -lt $errors_length ] && error=${errors[$(($code - 1))]}
 	echo "$i. Testing compilation of ../${tests[$i - 1]}.biplan"
-	[ $code -eq 1 ] && echo -e "Result: \033[32mpassed \033[m- Exit code: $code" || 
-	                   echo -e "Result: \033[31mfailed \033[m- Exit code: $code"
+	[ $code -eq 0 ] && echo -e "Result: \033[32mpassed \033[m- Exit code: $code" || 
+	                   echo -e "Result: \033[31mfailed \033[m- Exit code: $code ($error)"
 	echo " "
 done
