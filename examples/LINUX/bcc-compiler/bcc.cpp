feec404e83b0a91d6a9f929061c728bb0368e73a
gioblu <gioscarab@gmail.com>
2025-11-15 23:49:18 +0100

    bcc enhanced output

--- examples/LINUX/bcc-compiler/bcc.cpp
+++ examples/LINUX/bcc-compiler/bcc.cpp
@@ -4,9 +4,9 @@ char program[BCC_MAX_PROGRAM_SIZE];
 bcc_error_t bcc_error_callback = NULL; 
 
 void error_handler(uint16_t line, const char *position, const char *string) {
-  printf("| State    | \033[31mError: %s Line: %d", string, line);
+  printf("| State    |\033[31m ⚠️ Error: %s Line: %d", string, line);
   if(position != NULL) {
-    printf("\nCode: ");
+    printf(" Code: ");
     for(uint16_t i = 0; *position != 0 && *position != BP_LF; position++)
       printf("%c", *position);
   }
@@ -15,14 +15,14 @@ void error_handler(uint16_t line, const char *position, const char *string) {
 };
 
 int main(int argc, char* argv[]) {
-  printf("\n| BCC      | BIP Compiler Collection by Giovanni Blu Mitolo\n");
+  printf("\nBCC (BIP Compiler Collection) \nGiovanni Blu Mitolo 2025\n\n");
   FILE * p_file;
   long p_size;
   size_t result;
   // Open file
   p_file = fopen(argv[1], "r");
   if(p_file == NULL) {
-    printf("| State    | \033[31mError: Unable to open source file.\033[m\n\n");
+    printf("| State    |\033[31m ⚠️ Error: Unable to open source file.\033[m\n\n");
     exit(2);
   }
   // Obtain file size:
@@ -31,7 +31,7 @@ int main(int argc, char* argv[]) {
   rewind(p_file);
   
   if((sizeof(char) * p_size) >= BCC_MAX_PROGRAM_SIZE) {
-    printf("| State    | \033[31mError: ");
+    printf("| State    |\033[31m ⚠️ Error: ");
     printf(
       "Source too big, BCC_MAX_PROGRAM_SIZE is set to %dB.\033[m\n\n",
       BCC_MAX_PROGRAM_SIZE
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]) {
   // Copy the file into the buffer:
   result = fread(program, 1, p_size, p_file);
   if(result != p_size) {
-    printf("| State    | \033[31mError: Unable to read source file.\033[m\n\n");
+    printf("| State    |\033[31m ⚠️ Error: Unable to read source file.\033[m\n\n");
     exit(4);
   }
 
@@ -53,7 +53,7 @@ int main(int argc, char* argv[]) {
   bcc_error_callback = error_handler;
   uint32_t t = BPM_MICROS();
   if(!bcc_run(program)) {
-    printf("| State    | \033[31mError: Compilation failed.\033[m\n\n");
+    printf("| State    |\033[31m ⚠️ Error: Compilation failed.\033[m\n\n");
     exit(EXIT_FAILURE);
   }
   t = BPM_MICROS() - t;
@@ -61,7 +61,7 @@ int main(int argc, char* argv[]) {
   if(strlen(program) > 0) {
     FILE *o_file = fopen(argv[2], "w");
     if(o_file == NULL) {
-      printf("| State    | \033[31mError: Unable to open the target file.\033[m\n\n");
+      printf("| State    |\033[31m ⚠️ Error: Unable to open the target file.\033[m\n\n");
       exit(5);
     }
     fwrite(program, sizeof(char), strlen(program), o_file);
@@ -73,6 +73,6 @@ int main(int argc, char* argv[]) {
   
   printf("| Target   | %s (%dB)\n", argv[2], o_size);
   printf("| Duration | %.2f milliseconds \n", (float)(t) / 1000);
-  printf("| State    | \033[32mSuccess\033[m\n\n");
+  printf("| State    |\033[32m ✅ Success\033[m\n\n");
   exit(EXIT_SUCCESS);
 };
